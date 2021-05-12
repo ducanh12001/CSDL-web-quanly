1adc56663877e173efb60685ed3a224f348e3fb8
ducanh12001 <ducanh2001.da@gmail.com>
2021-05-12 15:18:59 +0700

    edit

--- CSDL_EnglishCenterWeb/settings.py
+++ CSDL_EnglishCenterWeb/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5sp4&*j(1o_p$h+u#sx5m0ee@r40h2!2$97h*g4%4la100d$n0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['trungtamanhngu.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
